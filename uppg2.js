a92d0dc60229e59e2ecc7d72d59942715508a0f3
Leon Kelmendi <103467549+ParoxGamer@users.noreply.github.com>
2025-06-26 13:20:53 +0200

    Update uppg2.js
    Uppgift 2 klar

--- uppg2.js
+++ uppg2.js
@@ -3,7 +3,9 @@ function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
 
-
+   const namn = "Leon";
+    console.log(namn);
+   
 }
 
 module.exports = { uppg2 };
